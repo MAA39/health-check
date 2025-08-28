4c19d574a6b87c4ffe1ea197afd42368f680d050
Masakazu Takahashi <55798814+MAA39@users.noreply.github.com>
2025-08-28 16:09:11 +0900

    feat: watchOS SwiftUIアプリ参考テンプレート追加

---
+++ templates/watchos/HealthCheckWatchApp.swift
@@ -0,0 +1,18 @@
+/*
+ * ヘルスチェック watchOS アプリ - メインエントリーポイント
+ * SwiftUI + WatchConnectivity 対応
+ * 
+ * 注意: このファイルは参考用です
+ * 実際は Xcode で "Watch App for iOS App" ターゲットを追加して作成してください
+ */
+
+import SwiftUI
+
+@main
+struct HealthCheckWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
